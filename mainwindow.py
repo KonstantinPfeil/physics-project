592e7dca578d2a57aa3768c1fb9891de6e21b800
Konstantin Pfeil <96192071+KonstantinPfeil@users.noreply.github.com>
2022-05-30 11:45:35 +0200

    Add files via upload

---
+++ mainwindow.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# This Python file uses the following encoding: utf-8
+# main_widget.py BSZET-DD Template
+# Copyright © 2022 by SRE
+
+import os
+import sys
+
+from PySide6.QtUiTools import loadUiType
+from PySide6 import QtCore as Core
+from PySide6 import QtWidgets
+
+UIFilename = "form.ui"
+ProjectDir = os.path.dirname(os.path.abspath(__file__))
+Form, Base = loadUiType(os.path.join(ProjectDir, UIFilename))
+
+
+class MainWindow(Base, Form):
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    Core.QCoreApplication.setAttribute(Core.Qt.AA_ShareOpenGLContexts)
+    app = QtWidgets.QApplication(sys.argv)
+    widget = MainWindow()
+    widget.show()
+
+    sys.exit(app.exec())
