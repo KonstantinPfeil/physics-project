b4e34deca9286070094a23d93bbd506ba6618f55
Wendt <t21wendtka>
2022-06-01 12:23:43 +0200

    diagramm test

---
+++ diagram.py
@@ -0,0 +1,8 @@
+import matplotlib
+import matplotlib.pyplot as plt
+#matplotlib.use('TkAgg')
+t = [0, 1.447, 3.582, 4.766, 5.705, 6.538, 7.305, 7.994, 8.631, 9.217, 9.738]
+v = [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+
+plt.plot(t, v)
+plt.show()
