a8bead510ce8585ca48a052b644e053d557153b2
CHr <christoph.trischler@t-online.de>
2022-06-02 14:19:49 +0200

    first calculations

--- diagram.py
+++ diagram.py
@@ -1,8 +1,29 @@
 import matplotlib
 import matplotlib.pyplot as plt
-#matplotlib.use('TkAgg')
-t = [0, 1.447, 3.582, 4.766, 5.705, 6.538, 7.305, 7.994, 8.631, 9.217, 9.738]
-v = [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+import numpy as np
+import json
 
-plt.plot(t, v)
-plt.show()
+with open("mtimes.txt") as f:
+    times = json.loads(f.read())["times"]   # read times from file
+
+weg = [(x+1)*0.20 for x, v in enumerate(times)]
+timeDifferences = [t - times[x - 1] for x, t in enumerate(times) if x > 0]
+speeds = [0.20/t for t in timeDifferences]
+speedDifferences = [v - speeds[x - 1] for x, v in enumerate(speeds) if x > 0]
+accelerations = [v / t for v, t in zip(speedDifferences, timeDifferences[1:])]
+meanAcceleration = sum(accelerations) / len(accelerations)
+firstSpeed = speeds[0] - np.sqrt(meanAcceleration * 2 * 0.2)
+speeds.insert(0, firstSpeed)
+print(firstSpeed)
+print(0.2 / firstSpeed)
+firstTimeDifference = times[0] - 0.2 / firstSpeed
+timeDifferences.insert(0, firstTimeDifference)
+times.insert(0, firstTimeDifference)
+"""for x, t in enumerate(times):
+    print(f"{x}: {t}")"""
+times = [t - times[0] for t in times]
+# print(times)
+timeDifferences.insert(0, firstTimeDifference)
+accelerations.insert(0, [None, None])   # dummy data
+""" Adding zero points"""
+
