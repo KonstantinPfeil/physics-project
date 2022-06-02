01ae6b149370d572d123c6152838712599936f25
ChrisrtophTrischler <73523526+ChrisrtophTrischler@users.noreply.github.com>
2022-06-02 15:20:20 +0200

    test plots

--- diagram.py
+++ diagram.py
@@ -7,23 +7,35 @@ with open("mtimes.txt") as f:
     times = json.loads(f.read())["times"]   # read times from file
 
 weg = [(x+1)*0.20 for x, v in enumerate(times)]
+weg.insert(0, 0)  # insert zero point
 timeDifferences = [t - times[x - 1] for x, t in enumerate(times) if x > 0]
+
 speeds = [0.20/t for t in timeDifferences]
 speedDifferences = [v - speeds[x - 1] for x, v in enumerate(speeds) if x > 0]
+
 accelerations = [v / t for v, t in zip(speedDifferences, timeDifferences[1:])]
 meanAcceleration = sum(accelerations) / len(accelerations)
-firstSpeed = speeds[0] - np.sqrt(meanAcceleration * 2 * 0.2)
-speeds.insert(0, firstSpeed)
-print(firstSpeed)
-print(0.2 / firstSpeed)
-firstTimeDifference = times[0] - 0.2 / firstSpeed
-timeDifferences.insert(0, firstTimeDifference)
-times.insert(0, firstTimeDifference)
-"""for x, t in enumerate(times):
-    print(f"{x}: {t}")"""
+
+firstTime = np.sqrt(0.4 / meanAcceleration)
+times.insert(0, firstTime)
 times = [t - times[0] for t in times]
-# print(times)
-timeDifferences.insert(0, firstTimeDifference)
-accelerations.insert(0, [None, None])   # dummy data
+
+speedDifferences.insert(0, times[1] - times[0])  # create new difference with new time
+
+speeds.insert(0, 0.2 / timeDifferences[0])  # first and second speed
+speeds.insert(0, 0.2/firstTime)
+
+accelerations.insert(0, None)# dummy data
+accelerations.insert(0, None)
+accelerations.insert(0, None)
 """ Adding zero points"""
 
+
+plt.plot(times, weg)
+plt.show()
+
+plt.plot(times, speeds)
+plt.show()
+
+plt.plot(times, accelerations)
+plt.show()
