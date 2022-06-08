4e9ecdb07e46802d5b65637ab647761c0827befd
DarkXilef <60404987+DarkXilef@users.noreply.github.com>
2022-06-08 12:23:05 +0200

    erwartete_diagramme ein error aktuell

--- diagram.py
+++ diagram.py
@@ -2,6 +2,7 @@ import os
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 def calculate():
     try:
@@ -14,17 +15,14 @@ def calculate():
     weg = [x * 0.20 for x in range(len(times) + 1)]
 
     timeDifferences = [t - times[x - 1] for x, t in enumerate(times) if x > 0]
-
     speeds = [0.20 / t for t in timeDifferences]
     speedDifferences = [v - speeds[x - 1] for x, v in enumerate(speeds) if x > 0]
-
     accelerations = [v / t for v, t in zip(speedDifferences, timeDifferences[1:])]
     meanAcceleration = sum(accelerations) / len(accelerations)
-
     firstTime = np.sqrt(0.4 / accelerations[0])
     times.insert(0, firstTime)
     times = [t - times[0] for t in times]
-
+    averageSpeed = [meanAcceleration * t for t in times]
     speedDifferences.insert(0, times[1] - times[0])  # create new difference with new time
 
     speeds.insert(0, 0.2 / timeDifferences[0])  # first and second speed
@@ -33,16 +31,32 @@ def calculate():
     accelerations.insert(0, meanAcceleration)  # dummy data
     accelerations.insert(0, meanAcceleration)
     accelerations.insert(0, meanAcceleration)
-    return times, weg, speeds, accelerations
+
+    return meanAcceleration, averageSpeed, times, weg, speeds, accelerations
+
 
 
 if __name__ == "__main__":
     calculation = calculate()
     if calculation is not None:
-        t, s, v, a = calculation
+        aAverage, vAverage, t, s, v, a = calculation
         plt.plot(t, s)
+        plt.xlabel("s")
+        plt.ylabel("t")
         plt.show()
         plt.plot(t, v)
+        plt.xlabel("t")
+        plt.ylabel("v")
         plt.show()
         plt.plot(t, a)
+        plt.xlabel("t")
+        plt.ylabel("a")
+        plt.show()
+        plt.plot(t, vAverage)
+        plt.xlabel("t")
+        plt.ylabel("v2")
+        plt.show()
+        plt.plot(t, aAverage)
+        plt.xlabel("t")
+        plt.ylabel("a2")
         plt.show()
