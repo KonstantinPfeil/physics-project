12b0598309aa98e5a9b221a0b5ab2123fc017092
DarkXilef <60404987+DarkXilef@users.noreply.github.com>
2022-06-10 10:20:06 +0200

    diagram laybel haben jetzt brüche und hochzeichen

--- diagram.py
+++ diagram.py
@@ -18,25 +18,26 @@ def calculate():
     speeds = [0.20 / t for t in timeDifferences]
     speedDifferences = [v - speeds[x - 1] for x, v in enumerate(speeds) if x > 0]
     accelerations = [v / t for v, t in zip(speedDifferences, timeDifferences[1:])]
-    meanAcceleration = sum(accelerations) / len(accelerations)
+    averageAcceleration = sum(accelerations) / len(accelerations)
     firstTime = np.sqrt(0.4 / accelerations[0])
     times.insert(0, firstTime)
     times = [t - times[0] for t in times]
-    averageSpeed = [meanAcceleration * t for t in times]
-    weg2 = [(meanAcceleration / 2) * t**2 for t in times ]
+    averageSpeed = [averageAcceleration * t for t in times]
+    predictedDistance = [(averageAcceleration / 2) * t**2 for t in times ]
     speedDifferences.insert(0, times[1] - times[0])  # create new difference with new time
 
       # first and second speed
     speeds.insert(0, 0.2 / firstTime)
     speeds.insert(0, 0)  # zero point
 
-    accelerations.insert(0, meanAcceleration)  # dummy data
-    accelerations.insert(0, meanAcceleration)
-    accelerations.insert(0, meanAcceleration)
+    accelerations.insert(0, averageAcceleration)  # dummy data
+    accelerations.insert(0, averageAcceleration)
+    accelerations.insert(0, averageAcceleration)
 
-    return weg2, meanAcceleration, averageSpeed, times, weg, speeds, accelerations
+    return averageSpeed, times, weg, predictedDistance, speeds, accelerations, averageAcceleration,
 
-def scatter(name: str, x, y, c: str, size: str):
+
+def scatter(name: str, x, y, c: str = None, size: str = None):
     plt.scatter(x,y, color = c, s = size)
     xname, yname = name.split("-")
     plt.xlabel(xname)
@@ -46,13 +47,13 @@ def scatter(name: str, x, y, c: str, size: str):
 if __name__ == "__main__":
     calculation = calculate()
     if calculation is not None:
-        s2, a2, v2, t, s, v, a = calculation
-        scatter("t in s -s in m",t,s, None, None)
+        v2, t, s, s2, v, a, aa = calculation
+        scatter("t in s -s in m",t,s)
         scatter(" t in s - s in m", t, s2, "red", 5)
         plt.show()
-        scatter("t-v", t, v, None, None)
-        scatter("t in s - v2 in m/s", t, v2, "red", 5)
+        scatter("t-v", t, v)
+        scatter(r"t in s - v in $\frac{m}{s}$", t, v2, "red", 5)
         plt.show()
-        scatter("t in s -a in m/s^2" , t, a, None, None)
-        scatter("t in s - a2 in m/s^2", t, [a2 for i in t], "red", 5)
+        scatter("t in s -a in m/s^2" , t, a)
+        scatter(r"t in s - a in $\frac{m}{s^{2}}$", t, [aa for i in t], "red", 5)
         plt.show()
