8aa339607b408de65feec796473082cd4d941824
CHr <christoph.trischler@t-online.de>
2022-06-11 01:34:46 +0200

    chart file

--- mainwindow.py
+++ mainwindow.py
@@ -10,13 +10,12 @@ import sys
 from PySide6.QtUiTools import loadUiType
 from PySide6 import QtCore as Core
 from PySide6 import QtWidgets
-from PySide6.QtCharts import QChart, QScatterSeries, QSplineSeries, QXYSeries, QValueAxis
 from PySide6.QtGui import QPainter, QShortcut, QKeySequence, QBrush, QColor
-from PySide6.QtCore import QPointF, Qt
 from PySide6.QtWidgets import QFileDialog
 
 from diagram import calculate
 from auslesen import readFromFile
+from chart import Chart, SplineSeries, ScatterSeries
 
 UIFilename = "form.ui"
 ProjectDir = os.path.dirname(os.path.abspath(__file__))
@@ -74,46 +73,6 @@ class MainWindow(Base, Form):
             print("wrong file")
             return
         self.setDiagrams()
-
-
-class Chart(QChart):
-    def __init__(self, series, title: str, maxX: int = 10,  maxY: int = 10):
-        super(Chart, self).__init__()
-        self.setTitle(title)
-        for s in series:
-            self.add(s)
-        self.legend().setVisible(True)
-        self.createDefaultAxes()
-        """ax = QValueAxis(self)
-        ax.setGridLineVisible(False)
-        ax.setRange(0, maxX)
-        self.addAxis(ax, Qt.AlignmentFlag.AlignBottom)
-        ay = QValueAxis(self)
-        ay.setGridLineVisible(False)
-        ay.setRange(0, maxY)
-        self.addAxis(ay, Qt.AlignmentFlag.AlignLeft)"""
-
-    def add(self, s: QXYSeries):
-        s.setParent(self)
-        self.addSeries(s)
-
-
-class SplineSeries(QSplineSeries):
-    def __init__(self, name, data):
-        super(SplineSeries, self).__init__()
-        self.setMarkerSize(5)
-        for x, y in data:
-            self << QPointF(x, y)
-        self.setName(name)
-
-
-class ScatterSeries(QScatterSeries):
-    def __init__(self, name, data):
-        super(ScatterSeries, self).__init__()
-        self.setMarkerSize(5)
-        for x, y in data:
-            self << QPointF(x, y)
-        self.setName(name)
 
 
 if __name__ == "__main__":
