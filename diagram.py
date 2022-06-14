calculation fix

diagram.py
@@ -2,7 +2,6 @@ import os
 import json
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
 
 def calculate(times: [float] = []):
@@ -11,40 +10,40 @@ def calculate(times: [float] = []):
             with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "mtimes.txt")) as f:
                 times = json.loads(f.read())["times"]  # read times from file
         except FileNotFoundError:
-            print("no mtimes.txt file \n try auslesen.py")
+            print("no mtimes.txt file \ntry auslesen.py")
             return None
         if not times:
             return None
+    times = np.asarray(times)
+    weg = np.linspace(0, 2, len(times) + 1)
 
-    weg = [x * 0.20 for x in range(len(times) + 1)]
+    timeDifferences = np.diff(times)
 
-    timeDifferences = [t - times[x - 1] for x, t in enumerate(times) if x > 0]
+    speeds = 0.2 / timeDifferences
+    speedDifferences = np.diff(speeds)
+    averageDeltaTime = timeDifferences[:-1]/2+timeDifferences[1:]/2
 
-    speeds = [0.20 / t for t in timeDifferences]
-    speedDifferences = [v - speeds[x - 1] for x, v in enumerate(speeds) if x > 0]
-
-    accelerations = [v / t for v, t in zip(speedDifferences, timeDifferences[1:])]
-    averageAcceleration = sum(accelerations) / len(accelerations)
+    accelerations = speedDifferences / averageDeltaTime
+    averageAcceleration = np.average(accelerations)
 
     firstTime = np.sqrt(0.4 / accelerations[0])
-    times.insert(0, firstTime)
-    times = [t - times[0] for t in times]
+    times = times - times[0] + firstTime
+    times = np.insert(times, 0, 0)
 
-    averageSpeed = [averageAcceleration * t for t in times]
-    predictedDistance = [(averageAcceleration / 2) * t**2 for t in times ]
-    speedDifferences.insert(0, times[1] - times[0])  # create new difference with new time
+    averageSpeed = averageAcceleration * times
+    predictedDistance = (averageAcceleration / 2) * times**2
+    speedDifferences = np.insert(speedDifferences, 0, times[1] - times[0])  # create new difference with new time
 
     # first and second speed
-    speeds.insert(0, 0.2 / firstTime)
-    speeds.insert(0, 0)  # zero point
+    speeds = np.insert(speeds, 0, 0.2 / firstTime)
+    speeds = np.insert(speeds, 0, 0)  # zero point
 
-    accelerations.insert(0, averageAcceleration)  # dummy data
-    accelerations.insert(0, averageAcceleration)
-    accelerations.insert(0, averageAcceleration)
+    for i in range(3):
+        accelerations = np.insert(accelerations, 0, averageAcceleration)  # dummy data
 
     averageAcceleration = [averageAcceleration for t in times]
 
-    return times, weg, speeds, accelerations, predictedDistance, averageSpeed, averageAcceleration,
+    return times, weg, speeds, accelerations, predictedDistance, averageSpeed, averageAcceleration
 
 
 def scatter(name: str, x, y, c: str = None, size: str = None):