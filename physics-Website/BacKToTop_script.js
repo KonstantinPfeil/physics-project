5860bc7d7adc6df8a58f3208f9735966b38de4ad
KonstantinPfeil <pfeilkonstantin@gmail.com>
2022-07-07 11:21:37 +0200

    imprint + FormUi(MenuBar)

--- physics-Website/BacKToTop_script.js
+++ physics-Website/BacKToTop_script.js
@@ -1,19 +1,20 @@
 let calcScrollValue = () => {
-    let scrollProgress = document.getElementById("progress");
-    let progressValue = document.getElementById("progress-value");
-    let pos = document.documentElement.scrollTop;
-    let calcHeight =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100) / calcHeight);
-    if (pos > 100) {
-      scrollProgress.style.display = "grid";
-    } else {
-      scrollProgress.style.display = "none";
-    }
-    scrollProgress.style.background = `conic-gradient(#bbb2e9 ${scrollValue}%, #3b3349 ${scrollValue}%)`;
-  };
-  window.onscroll = calcScrollValue;
-  window.onload = calcScrollValue;
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
   scrollProgress.addEventListener("click", () => {
-      document.documentElement.scrollTop = 0;});
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#bbb2e9 ${scrollValue}%, #3b3349 ${scrollValue}%)`;
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
